6575dc637b7db19f853d1885ed4859ffa626e821
Zephyr <zhentaop118@gmail.com>
2025-04-06 19:32:03 -0400

    Separated the DisplayPeriod section from the Home file to its own component

---
+++ stock-frontend/src/app/components/DisplayPeriod.js
@@ -0,0 +1,23 @@
+'use client'
+import { useState } from "react";
+
+export default function({ onSelectedPeriod }) {
+    const validPeriods = [
+        "1d", "5d", "1mo", "3mo", "6mo",
+        "1y", "2y", "5y", "10y", "ytd", "max"
+    ];
+
+    return (
+        <div>
+            <label>Select Period</label>
+
+            <select onChange={(e) => onSelectedPeriod(e.target.value)}>
+
+                <option key="default" value="">-- Select Period --</option>
+                {validPeriods.map((p) => (<option key={p} value={p}>{p}</option>))}
+
+            </select>
+
+        </div>
+    );
+}
--- stock-frontend/src/app/page.js
+++ stock-frontend/src/app/page.js
@@ -1,40 +1,19 @@
 'use client';
 import { useState } from "react";
 import StockInput from "./components/StockInput";
-
-function DisplayPeriod({ period, setPeriod }) {
-  const validPeriods = [
-    "1d", "5d", "1mo", "3mo", "6mo",
-    "1y", "2y", "5y", "10y", "ytd", "max"
-  ];
-
-  return (
-    <div>
-      <label>Select Period: </label>
-      <select 
-        value={period}
-        onChange={(e) => setPeriod(e.target.value)}
-      >
-        <option value="">-- Select Period --</option>
-        {validPeriods.map((p) => (<option value={p}>{p}</option>))}
-
-
-
-      </select>
-    </div>
-  )
-
-}
+import DisplayPeriod from "./components/DisplayPeriod";
 
 export default function Home() {
 
   const [validSymbol, setValidSymbol] = useState("");
+  const [selectedPeriod, setSelectedPeriod] = useState("");
 
   return (
     <main>
       <StockInput onValidSymbol={setValidSymbol}/>
+      <DisplayPeriod onSelectedPeriod={setSelectedPeriod}/>
 
-      {validSymbol && <DisplayPeriod />}
+      <p>Selected Period: {selectedPeriod}</p>
 
     </main>
   )
