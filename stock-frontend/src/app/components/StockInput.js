9c2498ee35739df2e065312f535f3caf5191c227
Zephyr <zhentaop118@gmail.com>
2025-04-08 01:02:54 -0400

    added alert pop up to inform of invalid stocks or backend errors; cleaned up and added comments to the StockInput.js file for better readability

--- stock-frontend/src/app/components/StockInput.js
+++ stock-frontend/src/app/components/StockInput.js
@@ -2,13 +2,17 @@
 import { useState } from "react";
 
 export default function StockInput({ onValidSymbol, resetPeriod,resetInterval }) {
+
     const [symbol, setSymbol] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
     const handleGoClick = async () => {
+
         if (!symbol) {
+
             alert("Please enter a stock symbol");
             return;
+        
         }
 
         if (isLoading) return;          // Prevent users from spamming when request still processing
@@ -17,39 +21,56 @@ export default function StockInput({ onValidSymbol, resetPeriod,resetInterval })
         console.log(`Stock symbol [${symbol}] sent to backend`);
 
         try {
+            
             const res =  await fetch(`http://localhost:5000/api/check_stock?symbol=${encodeURIComponent(symbol)}`);
 
-            if (!res.ok) {
-                throw new Error(`Server returned status ${res.status}`);
-            }
+            if (!res.ok) throw new Error(`Server returned status ${res.status}`);
             
             const data = await res.json();
 
             if (data.isValid) {
+
                 onValidSymbol(data.symbol);         // Set the symbol of the parent to valid
                 console.log(`Backend response: [${data.symbol}] is a [valid] stock symbol`);
+
             }else {
+
                 onValidSymbol("");                  // Invalid stock, empty string == False
                 console.log(`Backend response: [${data.symbol}] is an [invalid] stock symbol`);
+                alert("Invalid Stock")              // Pop up to alert the users of invalid stock symbol
+            
             }
+
         }
+
         catch (error){
+
             if (error.message == "Failed to fetch") {                   // When front can't communicate with back(AKA back is offline)
+                
                 console.error("Could not communicate with backend");
+                alert("Error communicating with backend server")
+            
             }else {                                                     // When back is online but error occurred
+                
                 console.error("Fetch failed:", error.message)
+                alert("Error:", error.message)
+            
             }
-            console.error("Stack trace:", error.stack);      
+            
+            console.error("Stack trace:", error.stack);
+                  
         } finally {
+
             setIsLoading(false);            // Set the loading status back to false after request is complete
             resetPeriod("")                 // Reset for new stock
             resetInterval("")               // Reset for new stock
+        
         }
+
     }
 
     return (
         
-
         <div className="flex items-center space-x-2 mt-4 mb-4">
 
             <input
@@ -62,10 +83,16 @@ export default function StockInput({ onValidSymbol, resetPeriod,resetInterval })
 
             <button
                 onClick={handleGoClick}
-                className={`px-4 py-2 font-semibold rounded-lg transition-colors duration-200 
-                    ${isLoading ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"}`}
+                className={
+                    `px-4 py-2 font-semibold rounded-lg transition-colors duration-200 
+                    ${isLoading 
+                        ? "bg-gray-400 cursor-not-allowed" 
+                        : "bg-blue-500 hover:bg-blue-600 text-white cursor-pointer"
+                    }`}
             >{isLoading ? "Loading..." : "Go"}</button> 
 
         </div>
+
     )
+    
 }
